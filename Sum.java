c8b639ad386ee7445e1e752a5cfbed6c4bbbf878
manasa6902 <111753412+manasa6902@users.noreply.github.com>
2024-03-13 16:08:35 +0530

    Sum.java

--- Sum.java
+++ Sum.java
@@ -3,7 +3,8 @@ public class Sum{
 	public static void main(String[] args){
 		int arr[]={10,51,53,39,47,44,21};
 		Arrays.sort(arr);
-		int ans=arr[5]+arr[4];
+		int n=arr.length;
+		int ans=arr[n-2]+arr[n-3];
 		System.out.print("Sum of 2nd and 3rd largest number is: "+ans);
 	}
 }
